b21ca10058d799d5fc8f1974a6cb29ebebe5d98d
Taleb27 <a7la.taleb.atta@gmail.com>
2022-03-07 13:08:15 +0200

    gg

--- app/src/main/java/y2022/aoc/taleb/final_try/tictoeActivity.java
+++ app/src/main/java/y2022/aoc/taleb/final_try/tictoeActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class tictoeActivity extends AppCompatActivity implements View.OnClickListener {
+public class tictoeActivity extends AppCompatActivity {
+private TextView playeronescore,playertwoscore,playerstatus;
+private Button [] buttons = new Button[9];
+private Button resetGame;
+private int playeronescorecount,playertwoscorecount,roundcount;
+boolean activePlayer;
 
+    int [] gameState = {2,2,2,2,2,2,2,2,2};
 
+    int [][]winningPositions = {
+            {0,1,2}, {3,4,5}, {6,7,8},//rows
+            {0,3,6}, {1,4,7} , {2,5,8}, // columns
+            {0,4,8} , {2,4,6}
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
