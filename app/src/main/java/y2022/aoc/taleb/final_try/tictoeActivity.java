d714aed5abd5c11117d8b1533a000732c29bfd50
Taleb27 <a7la.taleb.atta@gmail.com>
2022-03-09 13:05:09 +0200

    gg

--- app/src/main/java/y2022/aoc/taleb/final_try/tictoeActivity.java
+++ app/src/main/java/y2022/aoc/taleb/final_try/tictoeActivity.java
@@ -2,12 +2,14 @@ package y2022.aoc.taleb.final_try;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class tictoeActivity extends AppCompatActivity {
+public class tictoeActivity extends AppCompatActivity implements View.OnClickListener {
 private TextView playeronescore,playertwoscore,playerstatus;
 private Button [] buttons = new Button[9];
 private Button resetGame;
@@ -26,6 +28,94 @@ boolean activePlayer;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tictoe);
+        playeronescore =(TextView) findViewById(R.id.playeronescore);
+        playertwoscore = (TextView) findViewById(R.id.playertwoscore);
+        playerstatus = (TextView) findViewById(R.id.playerStatus);
+        resetGame = (Button) findViewById(R.id.resetGame);
+
+        for(int i=0; i< buttons.length;i++){
+            String buttonID = "btn_"+i;
+            int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            buttons[i] = (Button) findViewById(resourceID);
+            buttons[i].setOnClickListener(this);
+
+        }
+roundcount = 0;
+        playeronescorecount=0;
+        playertwoscorecount=0;
+        activePlayer=true;
+    }
+
+    @Override
+    public void onClick(View v) {
+      if (!((Button)v).getText().toString().equals("")){
+          return;
+      }
+      String buttonId = v.getResources().getResourceEntryName(v.getId());
+      int gameStatePointer = Integer.parseInt(buttonId.substring(buttonId.length()-1,buttonId.length()));
+
+      if(activePlayer){
+          ((Button) v).setText("x");
+          ((Button) v).setTextColor(Color.parseColor("#FFC34A"));
+          gameState[gameStatePointer] = 0;
+      } else{
+          ((Button) v).setText("o");
+          ((Button) v).setTextColor(Color.parseColor("#70FFEA"));
+          gameState[gameStatePointer] = 1;
+      }
+      roundcount++;
+      if (checkWinner()){
+          if (activePlayer){
+              playeronescorecount++;
+              updateplayerScore();
+              Toast.makeText(this,"player one Won!", Toast.LENGTH_SHORT).show();
+              playAgain();
+          }else{
+              playertwoscorecount++;
+              updateplayerScore();
+              Toast.makeText(this,"player two Won!", Toast.LENGTH_SHORT).show();
+              playAgain();
+          }
+      }else if (roundcount==9){
+             playAgain();
+             Toast.makeText(this, "No Winner!", Toast.LENGTH_SHORT).show();
+
+      }else {
+          activePlayer = !activePlayer;
+      }
+     if (playeronescorecount>playertwoscorecount){
+         playerstatus.setText("player One is winning!");
+     }else if (playeronescorecount<playertwoscorecount){
+         playerstatus.setText("player Two is winning!");
+     }else{
+         playerstatus.setText("");
+     }
+    }
+    public boolean checkWinner(){
+        boolean winnerResult = false;
+
+        for(int [] winningPosion : winningPositions){
+            if (gameState[winningPosion[0]] == gameState[winningPosion[1]]&& gameState[winningPosion[1]]== gameState[winningPosion[2]]&& gameState[winningPosion[0]]!=2)
+            {
+                winnerResult = true;
+            }
+        }
+        return winnerResult;
+    }
+public void updateplayerScore(){
+        playeronescore.setText(Integer.toString(playeronescorecount));
+        playertwoscore.setText(Integer.toString(playertwoscorecount));
+
+}
+public void playAgain(){
+        roundcount = 0;
+        activePlayer = true;
+
+        for (int i=0; i < buttons.length; i++){
+            gameState[i] = 2;
+            buttons[i].setText("");
+
+        }
 
     }
 }
